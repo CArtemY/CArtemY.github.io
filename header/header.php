7eeda4c468e95e566739d8d63a5ed5ed6f01743f
CArtemY <cherckasovartyom@gmail.com>
2019-07-16 23:42:42 +0300

    16.07.19

---
+++ header/header.php
@@ -0,0 +1,8 @@
+    <div class="header" id="header">
+        <div class="header_1">
+            <div class="header_ico" id="header_ico"></div>
+            <div class="header_logo" id="header_logo">MusicCloud</div>
+        </div>
+        <hr class="hr_header">
+    </div>
+    
---
+++ navigation/nav.php
@@ -0,0 +1,16 @@
+            <li><a href="../home/home.php">
+                <div class="footer_ico_1" id="footer_ico_1"></div>    
+            </a></li>
+
+
+            <li><a href="../likes/likes.php">
+                <div class="footer_ico_2" id="footer_ico_2"></div>    
+            </a></li>
+
+            <li><a href="../found/found.php">
+                <div class="footer_ico_3" id="footer_ico_3"></div>    
+            </a></li>
+
+            <li><a href="../sing-in/sing-in.php">
+                <div class="footer_ico_4" id="footer_ico_4"></div>    
+            </a></li>
